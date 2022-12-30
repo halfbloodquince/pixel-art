3e9005a87711d72fcd9e10b2b81b05e3a493df94
Matt Woods <mattwoodsmaths@gmail.com>
2022-12-30 09:46:55 +0000

    refactor data

---
+++ src/components/data.jsx
@@ -0,0 +1,29 @@
+export default [
+    {
+        numbers: [
+        3,3,3,3,7,7,3,3,3,3,
+        3,3,3,5,5,5,5,3,3,3,
+        3,3,9,9,9,9,9,9,3,3,
+        3,6,12,6,12,12,6,12,6,3,
+        3,6,12,12,10,10,12,12,6,3,
+        3,3,11,11,11,11,11,11,3,3,
+        7,2,2,4,4,4,11,2,2,7,
+        12,12,2,4,4,4,4,11,12,12,
+        12,12,12,8,4,4,8,12,12,12,
+        12,12,8,8,12,12,8,8,12,12
+        ], 
+        colours: [
+            {num:6, colour:"black"}, 
+            {num:2, colour:"darkgray"}, 
+            {num:4, colour:"lightgray"}, 
+            {num:12, colour:"white"},
+            {num:3, colour:"azure"}, 
+            {num:9, colour:"dodgerblue"}, 
+            {num:5, colour:"green"}, 
+            {num:7, colour:"yellow"}, 
+            {num:10, colour:"orange"}, 
+            {num:11, colour:"red"}, 
+            {num:8, colour:"saddlebrown"}
+        ]
+    },
+]
--- src/pages/Game.jsx
+++ src/pages/Game.jsx
@@ -1,29 +1,19 @@
 import { useEffect, useState } from "react"
+import data from '../components/data'
 
 export default function Game({mult}) {
     const [chickenDinner, setChickenDinner] = useState(false)
     const [paint, setPaint] = useState(null)
     const [border, setBorder] = useState(null)
     const [current, setCurrent] = useState(null)
-    const numbers = [
-        3,3,3,3,7,7,3,3,3,3,
-        3,3,3,5,5,5,5,3,3,3,
-        3,3,9,9,9,9,9,9,3,3,
-        3,6,12,6,12,12,6,12,6,3,
-        3,6,12,12,10,10,12,12,6,3,
-        3,3,11,11,11,11,11,11,3,3,
-        7,2,2,4,4,4,11,2,2,7,
-        12,12,2,4,4,4,4,11,12,12,
-        12,12,12,8,4,4,8,12,12,12,
-        12,12,8,8,12,12,8,8,12,12
-        ]
+
+    const numbers = data[0].numbers
+    const colours = data[0].colours
 
     const whiteText = ["black", "saddlebrown", "green", "red"]
 
     const num2 = numbers.map((num, i) => ({id:i, n:num, g:0}))
     const [gameState, setGameState] = useState(num2)
-
-    const colours = [{num:6, colour:"black"}, {num:2, colour:"darkgray"}, {num:4, colour:"lightgray"}, {num:12, colour:"white"},{num:3, colour:"azure"}, {num:9, colour:"dodgerblue"}, {num:5, colour:"green"}, {num:7, colour:"yellow"}, {num:10, colour:"orange"}, {num:11, colour:"red"}, {num:8, colour:"saddlebrown"}]
 
     const handlePaintClick = (event) => {
         // console.log(event.target.style.backgroundColor)
@@ -132,7 +122,7 @@ export default function Game({mult}) {
                         </div>
                     </div>
 
-                    {chickenDinner && <h1 className="chicken--dinner">YOU WIN!</h1>}
+                    {chickenDinner && <h1 className="chicken--dinner">YOU DID IT!</h1>}
                 </div>
         </div>
     )
